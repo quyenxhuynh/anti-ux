ba8b4304abc18dd8e07e44d6822629a0c01464cd
Quyen Huynh <quyenhuynh@Quyens-MacBook-Pro.local>
2020-09-26 13:33:21 -0400

    more heroku ...

--- anti_site/anti_site/settings.py
+++ anti_site/anti_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'fw4+ezg#taj1!y1u8d7y3*iu3)oge7&n(yz1-i%p5t%b@3vy37'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'qh8ar-antiux.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -118,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
