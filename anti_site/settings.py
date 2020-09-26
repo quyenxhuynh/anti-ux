9274aedc50f458b8e2e4cc1141eb2f7c946bf364
Quyen Huynh <quyenhuynh@Quyens-MacBook-Pro.local>
2020-09-26 13:43:08 -0400

    sigh

--- anti_site/settings.py
+++ anti_site/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'anti_app.apps.AntiAppConfig'
+    'anti_app.apps.AntiAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
