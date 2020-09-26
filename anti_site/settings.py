5de4b42a6289ea2ebd9b6b216694c75a67341ab2
Quyen Huynh <quyenhuynh@Quyens-MacBook-Pro.local>
2020-09-26 14:47:35 -0400

    hi

--- anti_site/settings.py
+++ anti_site/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,6 +72,8 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WSGI_APPLICATION = 'anti_site.wsgi.application'
 
